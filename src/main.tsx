157085484579c0e8d617b3201cc861097bf90550
Saerom <park.srome@gmail.com>
2025-08-29 01:28:21 -0500

    stream llm tokens

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import ConversationCanvas from './ConversationCanvas';
+// import ConversationCanvas from './ConversationCanvas';
+import Test from './Test';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <ConversationCanvas />
+    {/* <ConversationCanvas /> */}
+    <Test />
   </StrictMode>,
 )
