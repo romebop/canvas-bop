6984356ba95f98b43cd040583b0a35f928a12c8a
Saerom <park.srome@gmail.com>
2025-09-05 22:11:28 -0500

    init conversation graph

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import ConversationCanvas from './ConversationCanvas';
+import ConversationGraph from './ConversationGraph';
 // import Test from './Test';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <ConversationCanvas />
+    <ConversationGraph />
     {/* <div style={{ display: 'flex' }}>
       <Test />
       <Test />
